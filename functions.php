8e998b14e4e1ece80637bf92df2278383c27f8fa
hpmk <mk@happygrahak.com>
2019-05-20 14:44:27 +0530

    funcation file

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php 
 function mytheme(){
-
 	//style sheet
 	wp_enqueue_style('wp_style',get_stylesheet_uri());
 	wp_enqueue_style('boostap_file',get_template_directory_uri().'/css/bootstrap.css');
@@ -9,6 +8,5 @@ function mytheme(){
 }
 
 add_action('wp_enqueue_scripts','mytheme');
-
 
 ?>
