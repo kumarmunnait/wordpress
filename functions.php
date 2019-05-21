48467eecbfec65d39434a634e71d1187e3682690
Er. M K Gupta <munnakumar572@gmail.com>
2019-05-21 10:10:17 +0530

    funcation

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php 
+
+//ad style sheet into header section
 function mytheme(){
+
 	//style sheet
 	wp_enqueue_style('wp_style',get_stylesheet_uri());
 	wp_enqueue_style('boostap_file',get_template_directory_uri().'/css/bootstrap.css');
@@ -9,4 +12,32 @@ function mytheme(){
 
 add_action('wp_enqueue_scripts','mytheme');
 
+
+
+
+
+
+//add menu bar in wordpress admin panel 
+
+
+function Add_menu_bar(){
+     // menu register code
+  register_nav_menus(
+            array(
+                'primary-menu' => __('Primary  Menu Bar'),  // Id and Name
+                'footer-menu' => __('Footer Menu Bar')     // Id and Name
+            )
+    );
+  }
+// After making menus registration function next we have to attach with action hook as 'init'.
+// attach with action hook
+add_action('init', 'Add_menu_bar'); 
+
+
+
+
+
 ?>
+
+
+
